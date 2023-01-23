0113230e6b681a72a3cf1711de96f1b51ff80c1d
Hugo.M <hmaestracci@gaming.tech>
2023-01-23 11:29:06 +0100

    Added combat state

---
+++ cs.project07.pokemon/game/states/list/CombatState.cs
@@ -0,0 +1,50 @@
+﻿using cs.project07.pokemon.game.map;
+
+namespace cs.project07.pokemon.game.states.list
+{
+    public class CombatState : State
+    {
+        // Get player pokemon
+        // Get enemy pokemon
+        public CombatState(Game game) : base(game)
+        {
+            Init();
+        }
+
+        protected override void Init()
+        {
+            Name = "Combat";
+            InitCombat();
+        }
+
+        private void InitCombat()
+        {
+
+        }
+        
+        public override void HandleKeyEvent(ConsoleKey pressedKey)
+        {
+            switch (pressedKey)
+            {
+
+            }
+        }
+
+        public override void Update()
+        {
+            base.Update();
+
+            // Update childs
+            // ------ Map
+        }
+
+        public override void Render()
+        {
+            base.Render();
+
+            // Render childs
+            // ------ Map
+            
+        }
+    }
+}
