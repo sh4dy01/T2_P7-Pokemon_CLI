0878a2d557a38bcf676040a8cc9b043002d38d14
Hugo Maestracci <hmaestracci@gaming.tech>
2023-01-26 11:29:33 +0100

    Added unitary test project

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+using cs.project07.pokemon.game.entites;
+
+namespace TestProject1
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+
+            Player p;
+        }
+    }
+}
