feb0ef809c0955ce09664959842a1e66e9e30080
Hugo Maestracci <hmaestracci@gaming.tech>
2023-01-26 11:29:42 +0100

    Added PokedexEntry

---
+++ cs.project07.pokemon/game/entites/PokedexEntry.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cs.project07.pokemon
+{
+	public enum Element
+	{
+		NORMAL,
+		FIRE,
+		WATER,
+		GRASS,
+		ELECTRIC,
+		ICE,
+		FIGHTING,
+		POISON,
+		GROUND,
+		FLYING,
+		PSYCHIC,
+		BUG,
+		ROCK,
+		GHOST,
+		DRAGON,
+		DARK,
+		STEEL
+	};
+
+	public class PokedexEntry
+    {
+		private int _pokedexID;
+		private string _name;
+		private int _maxHealth;
+		private Element _element;
+		private Attack[] _attacks;
+
+		public PokedexEntry(int pokedexId, string name, Element element, int maxHealth, Attack[] attacks)
+        {
+			_pokedexID = pokedexId;
+			_name = name;
+			_maxHealth = maxHealth;
+			_element = element;
+			_attacks = attacks;
+        }
+    }
+}
