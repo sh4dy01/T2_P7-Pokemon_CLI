c5128c32fb3a3e3d3e33b846de368453ba958d00
KingPrime69 <quentin.ripot@free.fr>
2023-01-29 22:12:09 +0100

    start

---
+++ cs.project07.pokemon/game/states/list/InventoryState.cs
@@ -0,0 +1,23 @@
+﻿namespace cs.project07.pokemon.game.states.list
+{
+    public class InventoryState : State
+    {
+        public Game game;
+        public InventoryState(Game gameReceive) : base(gameReceive) 
+        { 
+            game = gameReceive;
+            Init();
+        }
+
+        protected override void Init()
+        {
+            Name = "Inventory";
+            InitInventory();
+        }
+
+        private void InitInventory()
+        {
+
+        }
+    }
+}
