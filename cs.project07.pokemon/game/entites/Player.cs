769dfeac9c0fb62ba15b444ef72b581298454f0a
KingPrime69 <quentin.ripot@free.fr>
2023-01-30 17:06:33 +0100

    player center cam mouvement

--- cs.project07.pokemon/game/entites/Player.cs
+++ cs.project07.pokemon/game/entites/Player.cs
@@ -9,6 +9,9 @@ namespace cs.project07.pokemon.game.entites
 {
     public class Player
     {
+        private PokemonListManager _pokemonsCaptured;
+
+        const int incrementMouvement = 2;
         public Vector2 playerPosition;
         private ConsoleColor BackgroundColor;
         private ConsoleColor ForegroundColor;
