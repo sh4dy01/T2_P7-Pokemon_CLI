02d7f3116e8dbf189343a221cfcea1e063ea821b
KingPrime69 <quentin.ripot@free.fr>
2023-01-30 15:03:43 +0100

    Game menu finish

---
+++ cs.project07.pokemon/game/states/list/GameMenuState.cs
@@ -0,0 +1,11 @@
+﻿using cs.project07.pokemon.game.states.gui;
+using cs.project07.pokemon.game.states.gui.managers;
+using System.Numerics;
+
+namespace cs.project07.pokemon.game.states.list
+{
+    //internal class GameMenuState : State
+    //{
+
+    //}
+}
