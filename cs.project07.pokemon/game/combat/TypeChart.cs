c04d53d532668d7bf81537a91d409452626e7312
hmaestracci <hmaestracci@gaming.tech>
2023-01-30 16:35:47 +0100

    Added ConsoleColor to Type

--- cs.project07.pokemon/game/combat/TypeChart.cs
+++ cs.project07.pokemon/game/combat/TypeChart.cs
@@ -4,6 +4,27 @@ namespace cs.project07.pokemon.game.combat
 {
     public class TypeChart
     {
+        public static readonly Dictionary<Type, ConsoleColor> TypeColor = new()
+        {
+            {Type.NORMAL, ConsoleColor.White},
+            {Type.FIRE, ConsoleColor.Red},
+            {Type.WATER, ConsoleColor.Blue},
+            {Type.ELECTRIC, ConsoleColor.Yellow},
+            {Type.GRASS, ConsoleColor.Green},
+            {Type.ICE, ConsoleColor.Cyan},
+            {Type.FIGHTING, ConsoleColor.DarkRed},
+            {Type.POISON, ConsoleColor.DarkMagenta},
+            {Type.GROUND, ConsoleColor.DarkYellow},
+            {Type.FLYING, ConsoleColor.DarkGray},
+            {Type.PSYCHIC, ConsoleColor.Magenta},
+            {Type.BUG, ConsoleColor.DarkGreen},
+            {Type.ROCK, ConsoleColor.DarkGray},
+            {Type.GHOST, ConsoleColor.DarkGray},
+            {Type.DRAGON, ConsoleColor.DarkGray},
+            {Type.DARK, ConsoleColor.DarkGray},
+            {Type.STEEL, ConsoleColor.DarkGray},
+        };
+        
         private static dynamic Chart { get; set; }
 
         public static void Init()
@@ -17,7 +38,6 @@ namespace cs.project07.pokemon.game.combat
 
         public static float GetDamageMultiplier(Type attack, Type defense)
         {
-            
             return Chart[attack.ToString().ToLower()][defense.ToString().ToLower()];
         }
     }
