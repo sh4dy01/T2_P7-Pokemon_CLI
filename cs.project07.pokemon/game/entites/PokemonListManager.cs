ba7a19ce6ebbd89f4573e7dac5217196f34c1397
hmaestracci <hmaestracci@gaming.tech>
2023-01-30 16:37:58 +0100

    Added current battle team

--- cs.project07.pokemon/game/entites/PokemonListManager.cs
+++ cs.project07.pokemon/game/entites/PokemonListManager.cs
@@ -9,10 +9,16 @@ namespace cs.project07.pokemon.game.entites
     internal class PokemonListManager
     {
         private readonly List<Pokemon> _pokemonCaptured;
+        private readonly Pokemon[] _battleTeam = new Pokemon[6];
 
         PokemonListManager()
         {
-            _pokemonCaptured= new List<Pokemon>();
+            _pokemonCaptured = new List<Pokemon>();
+        }
+
+        public void AddPokemon(Pokemon pokemon)
+        {
+            _pokemonCaptured.Add(pokemon);
         }
     }
 }
