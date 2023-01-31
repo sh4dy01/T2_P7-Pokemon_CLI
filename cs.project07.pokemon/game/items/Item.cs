183883e945727f3726a50080610e271219f982ce
Lum0o <gpipart@gmail.com>
2023-01-31 17:59:46 +0100

    new items

---
+++ cs.project07.pokemon/game/items/Item.cs
@@ -0,0 +1,24 @@
+﻿using cs.project07.pokemon.game.entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs.project07.pokemon.game.items
+{
+    public class Item
+    {
+        static protected char _id { get; set; }
+        protected int _quantity { get; set; }
+
+        public Item() { }   
+        virtual public void Use(Pokemon pokemon) {}
+        public void Add ( int quantity ) { _quantity += quantity; }
+        public void Remove(int quantity) { _quantity -= quantity; }
+        public int GetQuantity() { return _quantity; }
+        public virtual void SetQuantity( int quantity ) { _quantity = quantity; }
+
+    }
+}
---
+++ cs.project07.pokemon/game/items/list/Pokeball.cs
@@ -0,0 +1,29 @@
+﻿using cs.project07.pokemon.game.entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs.project07.pokemon.game.items.list
+{
+    internal class Pokeball : Item
+    {
+        public Pokeball()
+        {
+            _quantity = 0;
+            _id = 'B';
+        }
+        public Pokeball(int quantity)
+        {
+            _quantity = quantity;
+            _id = 'B';
+        }
+
+        override public void Use(Pokemon pokemon)
+        {
+            
+        }
+    }
+}
