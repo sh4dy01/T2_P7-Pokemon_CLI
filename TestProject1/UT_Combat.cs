9286b77d18dee463e6734f73c61c92f07ddb7e34
Hugo.M <h.maestracci83@gmail.com>
2023-02-01 12:08:11 +0100

    Critical UT test

--- TestProject1/UT_Combat.cs
+++ TestProject1/UT_Combat.cs
@@ -24,22 +24,32 @@ namespace UnitTest
         }
 
         [Test]
-        [TestCase(10, 20)]
-        [TestCase(1000, 0)]
-        [TestCase(10, 20)]
-        public void CheckIfPokemonHasCorrectLife(int hp, int expected)
+        [TestCase(100, 35, 65)]
+        [TestCase(100, -20, 100)]
+        [TestCase(0, 35, 0)]
+        [TestCase(-10, 35, 0)]
+        public void CheckIfPokemonHasCorrectLifeAfterAttack(int hp, int damage, int expected)
+        {
+            Pokemon pokemon = new(PokemonRegistry.GetRandomPokemon()); 
+            pokemon.InitHealth(hp);
+            
+            pokemon.TakeDamage(damage);
+            Assert.That(pokemon.Currenthealth, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [TestCase(200, 200, 20, 200)]
+        [TestCase(50, 99, 20, 70)]
+        [TestCase(75, 80, 20, 80)]
+        [TestCase(75, 80, -10, 75)]
+        [TestCase(-10, 80, 20, 0)]
+        public void CheckIfPokemonHealCorrectly(int hp, int maxHP, int heal, int expected)
         {
             Pokemon pokemon = new(PokemonRegistry.GetRandomPokemon());
-            Assert.That(pokemon.Currenthealth, Is.EqualTo(pokemon.MaxHealth));
+            pokemon.InitHealth(hp);
 
-            pokemon.TakeDamage(10);
-            Assert.That(pokemon.Currenthealth, Is.EqualTo(pokemon.MaxHealth - 10));
-
-            pokemon.TakeDamage(1000);
-            Assert.That(pokemon.Currenthealth, Is.EqualTo(0));
-
-            pokemon.Heal(1000);
-            Assert.That(pokemon.Currenthealth, Is.EqualTo(pokemon.MaxHealth));
+            pokemon.Heal(heal);
+            Assert.That(pokemon.Currenthealth, Is.EqualTo(expected));
         }
 
         [Test]
@@ -55,6 +65,21 @@ namespace UnitTest
             pokemon.GainExperience(1000);
             Assert.That(pokemon.Level, Is.EqualTo(9));
             Assert.That(pokemon.Experience, Is.EqualTo(120));
+        }
+
+        [Test]
+        public void IsAttackLandsACriticalHit()
+        {
+            Random rnd = new(057811);
+            
+            float[] attackerSpeed = { -10, 0, 15, 30, 45, 75, 95, 120, 140, 245, 255, 260, 210, 650, 714, 10000 };
+            bool[] expected = { false, false, false, false, true, false, false, false, true, true, true, true, false, true, true, true };
+
+            for (int i = 0; i < attackerSpeed.Length; i++)
+            {
+                bool isCritical = DamageCalculator.IsCritical(attackerSpeed[i], rnd, rnd.Next(0, 256)) > 1;
+                Assert.That(isCritical, Is.EqualTo(expected[i]));
+            }
         }
     }
 }
--- cs.project07.pokemon/game/entites/Pokemon.cs
+++ cs.project07.pokemon/game/entites/Pokemon.cs
@@ -15,12 +15,14 @@ namespace cs.project07.pokemon.game.entites
 
         private bool _isDead = false;
         private float _currentHealth;
+        private float _maxHealth;
         private int _level;
         private float _experience;
         private float _requiredExperience;
 
         private float? _maxEnemyHealth;
 
+        //TODO: Create a stat object to store all stats -> pokedex not settable / pokemon yes
 
         public bool IsDead { get => _isDead; set => _isDead = value; }
         public string Name { get => _dex.Name; }
@@ -28,7 +30,7 @@ namespace cs.project07.pokemon.game.entites
         public float Currenthealth { get => _currentHealth; }
         public float Experience { get => _experience; }
         public float RequiredExp { get => _requiredExperience; }
-        public float MaxHealth { get => _dex.MaxHealth; }
+        public float MaxHealth { get => _maxHealth; }
         public float Attack { get => _dex.Attack; }
         public float SPAttack { get => _dex.SPAttack; }
         public float Defense { get => _dex.Defense; }
@@ -45,6 +47,7 @@ namespace cs.project07.pokemon.game.entites
             _currentHealth = _dex.MaxHealth;
             _level = 1;
             _experience = 0;
+            _maxHealth = _dex.MaxHealth;
             _requiredExperience = LEVEL_UP_STEP;
         }
 
@@ -58,8 +61,17 @@ namespace cs.project07.pokemon.game.entites
             }
         }
 
+        public void InitHealth(int amount)
+        {
+            if (amount < 0) amount = 0;
+            
+            _currentHealth = amount;
+        }
+
         public void TakeDamage(float damage)
         {
+            if (damage < 0) return;
+            
             _currentHealth -= damage;
             if (_currentHealth <= 0)
             {
@@ -70,6 +82,9 @@ namespace cs.project07.pokemon.game.entites
 
         public void Heal(int amount)
         {
+            if (amount < 0) return;
+            if (_currentHealth <= 0) return;
+
             _currentHealth += amount;
             if (_currentHealth >= _dex.MaxHealth) _currentHealth = _dex.MaxHealth;
         }
