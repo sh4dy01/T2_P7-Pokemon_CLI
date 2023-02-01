cf4855215f7a7a403132386bb22287f67f5fa10f
KingPrime69 <quentin.ripot@free.fr>
2023-02-01 15:56:09 +0100

    Pokemon list

--- cs.project07.pokemon/game/Game.cs
+++ cs.project07.pokemon/game/Game.cs
@@ -46,7 +46,7 @@ namespace cs.project07.pokemon.game
 
         private void InitStates()
         {
-            StatesList?.Push(new CombatState(this));
+            StatesList?.Push(new InventoryState(this));
         }
 
         private void Init()
