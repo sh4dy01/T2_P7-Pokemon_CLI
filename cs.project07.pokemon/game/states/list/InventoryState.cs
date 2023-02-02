84b9622a1e6f2c4ae3558ca39bfa131dcefc84d7
KingPrime69 <quentin.ripot@free.fr>
2023-02-02 11:42:12 +0100

    box draw

--- cs.project07.pokemon/game/states/list/InventoryState.cs
+++ cs.project07.pokemon/game/states/list/InventoryState.cs
@@ -209,7 +209,8 @@ namespace cs.project07.pokemon.game.states.list
                         //showPokemonStats(pokemon, Xpos, Ypos);
                         showMoreStatPoke = numb;
                         //TO DO: SHOW Pokemon interface
-                    }
+                    },
+                    BackgroundColor = ConsoleColor.Gray
                 };
 
 
@@ -275,11 +276,11 @@ namespace cs.project07.pokemon.game.states.list
 
                 if(count == showMoreStatPoke)
                 {
-                    for (int i = 0; i <= 6; i++)
+                    for (int i = 0; i <= 9; i++)
                     {
                         for (int j = 0; j <= 47; j++)
                         {
-                            Console.SetCursorPosition((int)Xpos - increX - 3 + j, (int)Ypos + 6 + 1 * i);
+                            Console.SetCursorPosition((int)Xpos - increX - 3 + j, (int)Ypos + 6 + + 1 * i);
                             Console.WriteLine(" ");
                         }
                     }
