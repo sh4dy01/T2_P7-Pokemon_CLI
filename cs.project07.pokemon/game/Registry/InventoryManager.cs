0f93291b94e3a5e62bb191b9cc9875995162d299
Hugo.M <h.maestracci83@gmail.com>
2023-02-02 21:06:54 +0100

    added InventoryManager

---
+++ cs.project07.pokemon/game/Registry/InventoryManager.cs
@@ -0,0 +1,29 @@
+﻿using cs.project07.pokemon.game.items;
+
+namespace cs.project07.pokemon.game.Registry
+{
+    internal static class InventoryManager
+    {
+        private static List<Item> _inventory = new();
+
+        public static void AddItem(Item item)
+        {
+            if (_inventory.Contains(item))
+            {
+                _inventory.Find(x => x.getName() == item.getName()).Add(1);
+            }
+            else
+            {
+                _inventory.Add(item);
+            }
+        }
+
+        public static void RemoveItem(Item item)
+        {
+            if (_inventory.Contains(item))
+            {
+                _inventory.Find(x => x.getName() == item.getName()).Remove(1);
+            }
+        }
+    }
+}
