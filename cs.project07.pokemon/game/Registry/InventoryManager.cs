0d5c2ba5a0daa5ed4752f4683e3be285bbff3fe7
Hugo.M <h.maestracci83@gmail.com>
2023-02-02 21:12:42 +0100

    Init inventory

--- cs.project07.pokemon/game/Registry/InventoryManager.cs
+++ cs.project07.pokemon/game/Registry/InventoryManager.cs
@@ -1,20 +1,28 @@
 ﻿using cs.project07.pokemon.game.items;
+using cs.project07.pokemon.game.items.list;
 
 namespace cs.project07.pokemon.game.Registry
 {
     internal static class InventoryManager
     {
-        private static List<Item> _inventory = new();
+        private static List<Item> _inventory = new()
+        {
+            new Pokeball(0),
+            new Pokeball(1),
+            new Pokeball(2),
+            new Pokeball(3),
+            new Potion(0),
+            new Potion(1),
+            new Potion(2),
+            new Potion(3),
+            new Spray(),
+        };
 
         public static void AddItem(Item item)
         {
             if (_inventory.Contains(item))
             {
                 _inventory.Find(x => x.getName() == item.getName()).Add(1);
-            }
-            else
-            {
-                _inventory.Add(item);
             }
         }
 
